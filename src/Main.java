2baa89ce3e08120b76753442ec51bd24ca4e4dcd
Diovethi <pontetelmo8@gmail.com>
2021-10-20 09:43:59 +0100

    Update v2

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,7 @@ public class Main {
 
 			Instrutor inst1= new Instrutor("Roberta","26-10-1989","Femenino","936191001",espLista);
 
-<<<<<<< HEAD
 			ArrayList<Cliente> clienteLista= new ArrayList<Cliente>();
-=======
-
-			ArrayList<Cliente> clienteLista= new ArrayList<>();
->>>>>>> e7b6be29af61de2f544c0192d4908d31b3d629fd
 			clienteLista.add(new Cliente("Joao","13-05-1978","Masculino","teste@teste.com", espLista.get(1)));
 			clienteLista.add(new Cliente("Rodolfo","05-03-1998","Masculino","rodolfo@joka.pt", espLista.get(3)));
 			clienteLista.add(new Cliente("Roberta","23-10-1958","Feminino","Roberta@joka.com", espLista.get(2)));
